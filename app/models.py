7c23b8929fc366a99b90b4b67ad2ec6cf2fca205
hslim <hslim@nstepsoft.com>
2016-05-31 22:06:07 +0900

    파일추가
    파이썬 서버 테스트중

--- app/models.py
+++ app/models.py
@@ -1,11 +1,20 @@
 from app import db
+from passlib.apps import custom_app_context as pwd_context
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index = True)
+    password_hash = db.Column(db.String(128))
     coin = db.Column(db.Integer)
     gold = db.Column(db.Integer)
     nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,30 @@
+from app import db
 from app import app
+from .models import User
+from flask import Flask, abort, request, jsonify, g, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/api/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if User.query.filter_by(username = username).first() is not None:
+        abort(400) # existing user
+    user = User(username = username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({ 'username': user.username }), 201, {'Location': url_for('get_user', id = user.id, _external = True)}
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
